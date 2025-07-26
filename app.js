14ed9ea48fe08f52f8599f8291cf836d87f4ddde
Chandana jc <chandanajc659@gmail.com>
2025-07-26 12:34:10 +0530

    First web file.

---
+++ app.js
@@ -0,0 +1,17 @@
+(function () {
+'use Strict';
+
+var shoppingList = [
+  "Milk", "Donuts", "Cookies", "Chocolate", "peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
+];
+
+angular.module('ShoppingListApp', [])
+.controller('ShoppingListController', ShoppingListController);
+
+ShoppingListController.$inject = ['$scope'];
+function ShoppingListController($scope) {
+  $scope.shoppingList = shoppingList;
+}
+
+})();
+    
